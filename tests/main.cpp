3aa6dfc38ee8a984f8fe4de5cd6e64e71fb6e8e5
Matt McGurn <mtmcgurn@buffalo.edu>
2021-02-08 18:50:39 -0500

    debug

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,20 +3,20 @@
 #include "testFixtures/MpiTestEventListener.h"
 #include "testFixtures/MpiTestFixture.hpp"
 
-int main2(int argc, char** argv);
-
-int main(int argc, char** argv) {
-    return main2(argc, argv);
-    ::testing::InitGoogleTest(&argc, argv);
-
-    // Store the input parameters
-    const bool inMpiTestRun = MpiTestFixture::InitializeTestingEnvironment(&argc, &argv);
-    if (inMpiTestRun) {
-        testing::TestEventListeners& listeners = testing::UnitTest::GetInstance()->listeners();
-        delete listeners.Release(listeners.default_result_printer());
-
-        listeners.Append(new MpiTestEventListener());
-    }
-
-    return RUN_ALL_TESTS();
-}
+//int main2(int argc, char** argv);
+//
+//int main(int argc, char** argv) {
+//    return main2(argc, argv);
+//    ::testing::InitGoogleTest(&argc, argv);
+//
+//    // Store the input parameters
+//    const bool inMpiTestRun = MpiTestFixture::InitializeTestingEnvironment(&argc, &argv);
+//    if (inMpiTestRun) {
+//        testing::TestEventListeners& listeners = testing::UnitTest::GetInstance()->listeners();
+//        delete listeners.Release(listeners.default_result_printer());
+//
+//        listeners.Append(new MpiTestEventListener());
+//    }
+//
+//    return RUN_ALL_TESTS();
+//}
